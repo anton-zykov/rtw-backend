b454249bac98efdb157137dca54988a4b30eb563
Anton Zykov <anton.zykov1@gmail.com>
2025-10-05 02:11:19 +0200

    fix: change schema path in prisma.config to allow multi-file schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,5 +3,5 @@ import path from 'node:path';
 import type { PrismaConfig } from 'prisma';
 
 export default {
-  schema: path.join('prisma', 'schema', 'schema.prisma'),
+  schema: path.join('prisma', 'schema'),
 } satisfies PrismaConfig;
