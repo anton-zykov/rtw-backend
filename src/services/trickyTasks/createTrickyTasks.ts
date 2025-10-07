f824954d6b11b9bdaf52d3615c989bb848466c19
Anton Zykov <anton.zykov1@gmail.com>
2025-10-08 00:48:41 +0200

    feat: add createTrickyTasks service function

--- src/services/trickyTasks/createTrickyTasks.ts
+++ src/services/trickyTasks/createTrickyTasks.ts
@@ -1,1 +1,33 @@
-export async function createTrickyTasks () {}
+import { prisma } from '#/libs/prisma.js';
+import type { TrickyTask } from '@prisma/client';
+
+export type CreateTrickyTaskInput = {
+  age: number,
+  options: {
+    word: string,
+    correct: boolean
+  }[]
+}[];
+
+export async function createTrickyTasks (input: CreateTrickyTaskInput): Promise<TrickyTask[]> {
+  // TODO: think if validation is needed
+  const tasks = await prisma.trickyTask.createManyAndReturn({
+    data: input.map((item) => {
+      return {
+        age: item.age,
+        options: {
+          createMany: {
+            data: item.options.map((option) => {
+              return {
+                word: option.word,
+                correct: option.correct
+              };
+            })
+          }
+        }
+      };
+    })
+  });
+
+  return tasks;
+}
---
+++ test/unit/services/trickyTasks/createTrickyTasks.test.ts
@@ -0,0 +1,60 @@
+import { createTrickyTasks } from '#/services/trickyTasks/createTrickyTasks.js';
+import { prisma } from '#/libs/__mocks__/prisma.js';
+import { vi, beforeEach, test, expect } from 'vitest';
+import { mockReset } from 'vitest-mock-extended';
+import { randomUUID } from 'node:crypto';
+
+vi.mock('#/libs/prisma.js');
+
+beforeEach(() => {
+  mockReset(prisma);
+});
+
+test('should create tricky tasks', async () => {
+  const id1 = randomUUID();
+  const id2 = randomUUID();
+  const task1 = {
+    age: 1,
+    options: [
+      {
+        word: 'option1',
+        correct: true
+      },
+      {
+        word: 'option2',
+        correct: false
+      }
+    ]
+  };
+  const task2 = {
+    age: 2,
+    options: [
+      {
+        word: 'option3',
+        correct: true
+      },
+      {
+        word: 'option4',
+        correct: false
+      }
+    ]
+  };
+  prisma.trickyTask.createManyAndReturn.mockResolvedValue([
+    {
+      id: id1,
+      ...task1
+    }, {
+      id: id2,
+      ...task2
+    }
+  ]);
+
+  const tasks = await createTrickyTasks([{ ...task1 }, { ...task2 }]);
+  expect(tasks).toEqual([{
+    id: id1,
+    ...task1
+  }, {
+    id: id2,
+    ...task2
+  }]);
+});
