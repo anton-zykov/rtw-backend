160290adbecf5664dba5db8b545d0e126963105c
Anton Zykov <anton.zykov1@gmail.com>
2025-10-18 22:51:37 +0200

    test: add integration test for user update

--- test/integration/routes/user.test.ts
+++ test/integration/routes/user.test.ts
@@ -3,10 +3,10 @@ import { prismaPlugin } from '#/plugins/prisma.js';
 import { telegramPlugin } from '#/plugins/__mocks__/telegram.js';
 import type { PrismaClient } from '@prisma/client';
 
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest';
 import { mockDeep } from 'vitest-mock-extended';
 
-describe('user create', () => {
+describe('user routes', () => {
   let app: FastifyZodInstance;
   const prismaMock = mockDeep<PrismaClient>();
   Object.defineProperty(prismaMock, 'getter', { value: () => prismaMock });
@@ -30,103 +30,149 @@ describe('user create', () => {
 
   afterAll(async () => await app.close());
 
-  it('creates a user with login only and doesn\'t expose password hash', async () => {
-    prismaMock.user.create.mockResolvedValue({
-      id: 1,
-      login: 'Rich',
-      fullName: null,
-      email: null,
-      telegramId: null,
-      passwordHash: 'placeholder',
-      passwordVersion: 0,
-      createdAt: new Date(),
-      updatedAt: new Date()
+  describe('user create', () => {
+    afterEach(() => prismaMock.user.create.mockReset());
+
+    it('creates a user with login only and doesn\'t expose password hash', async () => {
+      prismaMock.user.create.mockResolvedValue({
+        id: 1,
+        login: 'Rich',
+        fullName: null,
+        email: null,
+        telegramId: null,
+        passwordHash: 'placeholder',
+        passwordVersion: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+
+      const res = await app.inject({
+        method: 'POST',
+        url: '/api/user/create',
+        payload: { login: 'Rich' },
+      });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.json()).toStrictEqual({
+        id: 1,
+        login: 'Rich',
+        fullName: null,
+        email: null,
+        telegramId: null,
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      });
     });
 
-    const res = await app.inject({
-      method: 'POST',
-      url: '/api/user/create',
-      payload: { login: 'Rich' },
-    });
-
-    expect(res.statusCode).toBe(201);
-    expect(res.json()).toStrictEqual({
-      id: 1,
-      login: 'Rich',
-      fullName: null,
-      email: null,
-      telegramId: null,
-      createdAt: expect.any(String),
-      updatedAt: expect.any(String)
-    });
-  });
-
-  it('creates a user with all fields', async () => {
-    prismaMock.user.create.mockResolvedValue({
-      id: 1,
-      login: 'Rich',
-      fullName: 'Richard',
-      email: 'exa@ple.com',
-      telegramId: '@rich',
-      passwordHash: 'placeholder',
-      passwordVersion: 0,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    });
-
-    const res = await app.inject({
-      method: 'POST',
-      url: '/api/user/create',
-      payload: {
+    it('creates a user with all fields', async () => {
+      prismaMock.user.create.mockResolvedValue({
+        id: 1,
         login: 'Rich',
         fullName: 'Richard',
         email: 'exa@ple.com',
-        telegramId: '@rich'
-      },
+        telegramId: '@rich',
+        passwordHash: 'placeholder',
+        passwordVersion: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+
+      const res = await app.inject({
+        method: 'POST',
+        url: '/api/user/create',
+        payload: {
+          login: 'Rich',
+          fullName: 'Richard',
+          email: 'exa@ple.com',
+          telegramId: '@rich'
+        },
+      });
+
+      expect(res.statusCode).toBe(201);
+      expect(res.json()).toStrictEqual({
+        id: 1,
+        login: 'Rich',
+        fullName: 'Richard',
+        email: 'exa@ple.com',
+        telegramId: '@rich',
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      });
     });
 
-    expect(res.statusCode).toBe(201);
-    expect(res.json()).toStrictEqual({
-      id: 1,
-      login: 'Rich',
-      fullName: 'Richard',
-      email: 'exa@ple.com',
-      telegramId: '@rich',
-      createdAt: expect.any(String),
-      updatedAt: expect.any(String)
+    it('fails to create a user with short <3 characters login', async () => {
+      const res = await app.inject({
+        method: 'POST',
+        url: '/api/user/create',
+        payload: { login: 'AB' },
+      });
+
+      expect(res.statusCode).toBe(400);
+      expect(res.json()).toMatchObject({ code: 'FST_ERR_VALIDATION' });
+    });
+
+    it('fails to create a user with no login', async () => {
+      const res = await app.inject({
+        method: 'POST',
+        url: '/api/user/create',
+        payload: {},
+      });
+
+      expect(res.statusCode).toBe(400);
+    });
+
+    it('doesn\'t allow custom id', async () => {
+      const res = await app.inject({
+        method: 'POST',
+        url: '/api/user/create',
+        payload: {
+          id: 2,
+          login: 'Rich'
+        },
+      });
+
+      expect(res.statusCode).toBe(400);
     });
   });
 
-  it('fails to create a user with short <3 characters login', async () => {
-    const res = await app.inject({
-      method: 'POST',
-      url: '/api/user/create',
-      payload: { login: 'AB' },
+  describe('user update', () => {
+    afterEach(() => prismaMock.user.update.mockReset());
+
+    it('updates a user', async () => {
+      prismaMock.user.update.mockResolvedValue({
+        id: 1,
+        login: 'Rich',
+        fullName: 'Richard',
+        email: 'exa@ple.com',
+        telegramId: '@rich',
+        passwordHash: 'placeholder',
+        passwordVersion: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+
+      const res = await app.inject({
+        method: 'PATCH',
+        url: '/api/user/update',
+        payload: {
+          id: 1,
+          login: 'Rich',
+          fullName: 'Richard',
+          email: 'exa@ple.com',
+          telegramId: '@rich'
+        },
+      });
+
+      expect(res.statusCode).toBe(200);
+      expect(res.json()).toStrictEqual({
+        id: 1,
+        login: 'Rich',
+        fullName: 'Richard',
+        email: 'exa@ple.com',
+        telegramId: '@rich',
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      });
     });
-
-    expect(res.statusCode).toBe(400);
-  });
-
-  it('fails to create a user with no login', async () => {
-    const res = await app.inject({
-      method: 'POST',
-      url: '/api/user/create',
-      payload: {},
-    });
-
-    expect(res.statusCode).toBe(400);
-  });
-
-  it('doesn\'t allow custom id', async () => {
-    const res = await app.inject({
-      method: 'POST',
-      url: '/api/user/create',
-      payload: {
-        id: 2,
-        login: 'Rich'
-      },
-    });
-
-    expect(res.statusCode).toBe(400);
   });
 });
