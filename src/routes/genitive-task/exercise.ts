12ec430371086ff4d4ccbb513b4a59846f0452c1
Anton Zykov <anton.zykov1@gmail.com>
2025-11-05 16:42:29 +0100

    feat: add route and service for genitive exercise genration with random task selection in the database

--- src/routes/genitive-task/exercise.ts
+++ src/routes/genitive-task/exercise.ts
@@ -1,5 +1,38 @@
+import { z } from 'zod';
+import { selectGenitiveTasksForExercise } from '#/services/genitiveTask/selectGenitiveTasksForExercise.js';
 import type { FastifyZodInstance } from '#/server.js';
 
+const GetGenitiveExerciseBody = z.object({
+  studentId: z.number().int().positive(),
+  amount: z.number().int().positive().optional(),
+}).strict();
+
+const GetGenitiveExerciseReply = z.array(
+  z.object({
+    taskId: z.uuid(),
+    weight: z.number().int().positive(),
+    nominative: z.string(),
+    options: z.unknown().pipe(
+      z.array(
+        z.object({
+          word: z.string(),
+          correct: z.boolean(),
+        })
+      )
+    ),
+  })
+);
+
 export async function genitiveTaskExerciseRoutes (app: FastifyZodInstance) {
-  // TODO /genitive/exercise
+  app.post('/get', {
+    schema: {
+      body: GetGenitiveExerciseBody,
+      response: {
+        200: GetGenitiveExerciseReply,
+      },
+    },
+  }, async (req, reply) => {
+    const tasks = await selectGenitiveTasksForExercise(app.prisma, req.body);
+    reply.status(200).send(tasks);
+  });
 }
---
+++ src/services/genitiveTask/selectGenitiveTasksForExercise.ts
@@ -0,0 +1,25 @@
+import { PrismaClient, type GenitiveTask, type StudentGenitiveTask } from '@prisma/client';
+
+export type SelectGenitiveTasksForExerciseInput = {
+  studentId: number;
+  amount?: number;
+};
+
+type StudentGenitiveTaskWithTaskDetails =
+  Pick<StudentGenitiveTask, 'taskId' | 'weight'> &
+  Pick<GenitiveTask, 'nominative' | 'options'>;
+
+export async function selectGenitiveTasksForExercise (
+  prisma: PrismaClient,
+  input: SelectGenitiveTasksForExerciseInput
+): Promise<StudentGenitiveTaskWithTaskDetails[]> {
+  const tasks = await prisma.$queryRaw<StudentGenitiveTaskWithTaskDetails[]>`
+    SELECT "taskId", "weight", "nominative", "options" FROM "StudentGenitiveTask"
+    JOIN "GenitiveTask" ON "StudentGenitiveTask"."taskId" = "GenitiveTask"."id"
+    WHERE "studentId" = ${input.studentId}
+    ORDER BY -LN(RANDOM()) / weight
+    LIMIT ${input.amount || 10};
+  `;
+
+  return tasks;
+}
