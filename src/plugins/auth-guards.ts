2b66b9b81e06c28a1fd87fc035369f4f58ed9580
Anton Zykov <anton.zykov1@gmail.com>
2025-12-21 18:12:48 +0100

    feat: add canModifyStudent guard, refactor other guards for more consistent errors

--- src/plugins/auth-guards.ts
+++ src/plugins/auth-guards.ts
@@ -1,4 +1,5 @@
 import fp from 'fastify-plugin';
+import { findStudentById } from '#/services/student/index.js';
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -6,64 +7,80 @@ declare module 'fastify' {
     requireAdmin: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
     requireStudent: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
     requireTeacher: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
+    canModifyStudent: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
     requireOwnerStudent: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
+  }
+
+  interface FastifyReply {
+    forbidden: (this: FastifyReply) => Promise<void>;
+    unauthorized: (this: FastifyReply) => Promise<void>;
   }
 }
 
 export const authGuardPlugin = fp(async (app) => {
+  app.decorateReply(
+    'forbidden',
+    async function forbidden () {
+      return this.code(403).send({ message: 'Forbidden' });
+    }
+  );
+
+  app.decorateReply(
+    'unauthorized',
+    async function unauthorized () {
+      return this.code(401).send({ message: 'Unauthorized' });
+    }
+  );
+
   app.decorate(
     'requireSession',
-    async function requireSession (request, reply) {
-      if (!request.session.userId) {
-        return reply.code(401).send({ message: 'Unauthorized' });
-      }
+    async function requireSession (req, reply) {
+      if (!req.session.userId) return reply.unauthorized();
     }
   );
 
   app.decorate(
     'requireAdmin',
-    async function requireAdmin (request, reply) {
-      if (!request.session.userId) {
-        return reply.code(401).send({ message: 'Unauthorized' });
-      }
-      if (request.session.role !== 'admin') {
-        return reply.code(403).send({ message: 'Forbidden' });
-      }
+    async function requireAdmin (req, reply) {
+      if (!req.session.userId) return reply.unauthorized();
+      if (req.session.role !== 'admin') return reply.forbidden();
     }
   );
 
   app.decorate(
     'requireStudent',
-    async function requireStudent (request, reply) {
-      if (!request.session.userId) {
-        return reply.code(401).send({ message: 'Unauthorized' });
-      }
-      if (request.session.role !== 'student') {
-        return reply.code(403).send({ message: 'Forbidden' });
-      }
+    async function requireStudent (req, reply) {
+      if (!req.session.userId) return reply.unauthorized();
+      if (req.session.role !== 'student') return reply.forbidden();
     }
   );
 
   app.decorate(
     'requireTeacher',
-    async function requireTeacher (request, reply) {
-      if (!request.session.userId) {
-        return reply.code(401).send({ message: 'Unauthorized' });
-      }
-      if (request.session.role !== 'teacher') {
-        return reply.code(403).send({ message: 'Forbidden' });
-      }
+    async function requireTeacher (req, reply) {
+      if (!req.session.userId) return reply.unauthorized();
+      if (req.session.role !== 'teacher') return reply.forbidden();
+    }
+  );
+
+  app.decorate(
+    'canModifyStudent',
+    async function canModifyStudent (req, reply) {
+      if (!req.session.userId) return reply.unauthorized();
+      if (req.session.role === 'admin') return;
+      if (req.session.role !== 'teacher') return reply.forbidden();
+      const teacherId = req.session.userId;
+      const student = await findStudentById(app.prisma, { id: (req.body as { studentId: string }).studentId });
+      if (student?.teacherId !== teacherId) return reply.forbidden();
     }
   );
 
   app.decorate(
     'requireOwnerStudent',
-    async function requireOwnerStudent (request, reply) {
-      if (!request.session.userId) {
-        return reply.code(401).send({ message: 'Unauthorized' });
-      }
-      if (request.session.role !== 'student' || request.session.userId !== (request.body as { studentId?: string }).studentId) {
-        return reply.code(403).send({ message: 'Forbidden' });
+    async function requireOwnerStudent (req, reply) {
+      if (!req.session.userId) return reply.unauthorized();
+      if (req.session.role !== 'student' || req.session.userId !== (req.body as { studentId?: string }).studentId) {
+        return reply.forbidden();
       }
     }
   );
